2441388bc2d6b30585b2a8257c6ee307eb13e0cb
LaikaFusion <newrad4@gmail.com>
2019-05-07 14:29:48 -0400

    used spread to flatten employee array

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const axios = require("axios");
 
 const apiUrl = "https://promise-hiring.appspot.com/employees";
 
-let employeeList = [];
+
 
 //helper function to avoid rate limiting
 const delay = interval => new Promise(resolve => setTimeout(resolve, interval));
 
-async function getAll(cursorStr = "") {
+async function getAll(cursorStr = "", employeeList = []) {
   return axios
     .get(apiUrl, {
       params: {
@@ -17,17 +17,29 @@ async function getAll(cursorStr = "") {
     })
     .then(async function(response) {
       const { data } = response;
-      employeeList.push(data.employees);
+      employeeList.push(...data.employees);
       if (data.cursor) {
         //there is a rate limiter on the api
         await delay(1000);
-        return getAll(data.cursor);
+        return getAll(data.cursor,employeeList);
+      }
+      else{
+        return employeeList
       }
     })
     .catch(function(error) {
       console.log(error);
     });
 }
-getAll().then(() => {
-  debugger;
+getAll().then((test) => {
+  console.log(test)
+  debugger
 });
+
+
+// function employeeListToObject(employeeList){
+//   let employeeObject ={};
+//   employeeList.forEach(element => {
+    
+//   });
+// }
