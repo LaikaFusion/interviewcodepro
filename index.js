e1c9657e7cf1566685f22db340307453871e3181
LaikaFusion <newrad4@gmail.com>
2019-05-08 17:31:51 -0400

    commented

--- index.js
+++ index.js
@@ -45,9 +45,13 @@ function employeeListToObject(employeeList) {
   return employeeObject;
 }
 
+
+//this removes all users that are only ever located inside someone's reports/managed_by and replaces it with a reference while adding the person it found to the main employee object
+//note temamates are not checked here due to the fact this does not happen in the data but could easily be added.
 function ensureCompleteList(employeeObj) {
   for (const key in employeeObj) {
     let currEmployee = employeeObj[key];
+
     if (currEmployee.hasOwnProperty("reports")) {
       employeeObj[key].is_manager = true;
       currEmployee.reports.forEach((e, i) => {
@@ -60,11 +64,12 @@ function ensureCompleteList(employeeObj) {
         }
       });
     }
+    
     if (currEmployee.hasOwnProperty("managed_by")) {
       const { managed_by } = currEmployee;
       //results don't seem to be standardized if they are in an array or not (ID 5035 in sample set for example)
       //this check is getting around how JS deals with arrays of objects
-      //since this only happens once I am assuming it is a mistake in the orginal data set
+      //since this only happens once I am assuming it is a mistake in the orginal data set and removing it
       if (Array.isArray(currEmployee.managed_by)) {
         employeeObj[key]["managed_by"] = currEmployee.managed_by[0];
       } else {
@@ -81,15 +86,18 @@ function ensureCompleteList(employeeObj) {
   }
   return employeeObj;
 }
-
+//this makes it so all connections in the data set are mutual. If someone is listed in a reported list, then they also have a managed_by field pointing to that person. 
+//Every field that can contain a relation is checked, reports, managed_by, and team_memebers
 function standardizeManagers(employeeObj) {
   for (const key in employeeObj) {
     let employee = employeeObj[key];
     if (employee.hasOwnProperty("reports")) {
       employee.reports.forEach(e => {
         let childCheck = employeeObj[e.ref_id];
-        //need to double check this for data
-        if (e.ref_id === 5050) {
+        //this is a filter for 
+        //"you should ignore any reference that does not resolve" -Shane O'Sullivan
+        //at this point anybody with an email should be in the eployee object
+        if (!(Object.keys(employeeObj).includes(e.ref_id.toString()))) {
           return;
         }
         if (!childCheck.hasOwnProperty("managed_by")) {
@@ -166,9 +174,12 @@ function standardizeManagers(employeeObj) {
   return employeeObj;
 }
 
+//this starts the heirarchy, but does the hard work is done by insertReports
 function constructHeirarchy(employeeObj) {
   let top = [];
   for (const key in employeeObj) {
+    //intially this also checked for reports field but //"anyone without a manager should go at the top" -Shane O'Sullivan
+    //There is an instance of a person who is not referenced by anyone else 
     if (!employeeObj[key].hasOwnProperty("managed_by")) {
       top.push({
         id: employeeObj[key].id,
@@ -178,11 +189,12 @@ function constructHeirarchy(employeeObj) {
       });
     }
   }
+  //this will return an array, which is the expected format for the submit
   return top.map(e => {
     return insertReports(employeeObj, e.id);
   });
 }
-
+//the hardworking part, a recursive function that fills every reports area with objects and the details of them, then their reports and so on
 function insertReports(employeeObj, employeeID) {
   let newObj = {
     id: employeeID,
@@ -192,6 +204,7 @@ function insertReports(employeeObj, employeeID) {
   if (employeeObj[employeeID].hasOwnProperty("reports")) {
     newObj.reports = employeeObj[employeeID].reports
       .filter(e => {
+        //this is a check for that unreferenced person and to avoid an error when their data is attempted to be read
         if (Object.keys(employeeObj).includes(e.ref_id.toString())) {
           return e;
         }
@@ -202,7 +215,7 @@ function insertReports(employeeObj, employeeID) {
   }
   return newObj;
 }
-
+//adds the rquired data for submission right to the objects themselves. both hash and grav url
 function hashEmailsandGrav(employeeObj) {
   for (const key in employeeObj) {
     const hash = crypto
@@ -215,49 +228,37 @@ function hashEmailsandGrav(employeeObj) {
   return employeeObj;
 }
 
-// getAll().then(test => {
-//   let employeeObj = employeeListToObject(test);
-//   employeeObj = ensureCompleteList(employeeObj);
-//   employeeObj = standardizeManagers(employeeObj);
-//   employeeObj = hashEmailsandGrav(employeeObj);
-//   employeeObj = constructHeirarchy(employeeObj);
-//   console.log(employeeObj);
-//   var fs = require("fs");
-//   fs.writeFileSync("myjsonfile.json", JSON.stringify(employeeObj), "utf8");
-//   debugger;
-
-//   axios
-//     .post("https://promise-hiring.appspot.com/result", {
-//       employees: employeeObj
-//     })
-//     .then(function(response) {
-//       console.log(response);
-//       debugger;
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//     });
-// });
-
-
 async function mainRequest (){
+  //retreives the data via axios, async
   let employeeObj = await getAll();
+
+  //takes the raw data and formats it into an easier to work with object
   employeeObj = employeeListToObject(employeeObj);
+
+  //this makes sure every employee with details is represented in the object and turns all reports/managed_bys to teammates
   employeeObj = ensureCompleteList(employeeObj);
+
+  //makes it so every employee has their managed_by, and reports fields filled out
   employeeObj = standardizeManagers(employeeObj);
+
+  //now that all employees are in the object this adds email_hashes, and gravavs to each object to get ready for submission
   employeeObj = hashEmailsandGrav(employeeObj);
+
+  //the actual heirachy of objects is constructed here
   employeeObj = constructHeirarchy(employeeObj);
-  console.log(employeeObj)
+
+  // the final submission
+
   axios
     .post("https://promise-hiring.appspot.com/result", {
       employees: employeeObj
     })
     .then(function(response) {
-      console.log(response);
-      debugger;
+      console.log(response.data);
     })
     .catch(function(error) {
       console.log(error);
     });
 }
+
 mainRequest();
